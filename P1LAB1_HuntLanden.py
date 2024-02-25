80af98a5400b7a5f800681685d89d59f964906ad
Landenhunt <160425868+Landenhunt@users.noreply.github.com>
2024-02-25 18:56:14 -0500

    Add files via upload

---
+++ P1LAB1_HuntLanden.py
@@ -0,0 +1,8 @@
+ # Landen Hunt
+ # 2/20/2024
+ # P1LAB1
+ # Add a title comment block to the top of the new Python file, to become familiar with comments in python
+print('Please enter yor name:', end='')
+name = input()
+print('Hey', name)
+print('Welcome to zybooks!')
