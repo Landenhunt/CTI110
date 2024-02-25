56468d9f64d1275373684fbd0cf88d371bbd4220
Landenhunt <160425868+Landenhunt@users.noreply.github.com>
2024-02-25 18:48:20 -0500

    Add files via upload

---
+++ P1HW1_HuntLanden.py
@@ -0,0 +1,17 @@
+# Landen Hunt
+# 2/25/2024
+# P1HW1
+# Using Python's input and print functions, we will create a program
+user_num = int(input('Enter integer:\n'))
+
+print ('You entered:', user_num)
+
+print (user_num, 'squared is', user_num * user_num)
+
+print('And', user_num,'cubed is', user_num * user_num * user_num, '!!')
+
+user_num2 = int(input('Enter another integer:\n'))
+
+print(user_num, "+", user_num2,'is',user_num + user_num2)
+
+print(user_num, "*", user_num2,'is',user_num * user_num2)
